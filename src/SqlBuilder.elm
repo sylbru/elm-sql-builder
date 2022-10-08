df485af738fcff32adf33d2620f22982c0087981
sylbru <sylvain.brunerie@gmail.com>
2022-10-09 00:03:16 +0200

    Rename Column to SelectExpression

--- src/SqlBuilder.elm
+++ src/SqlBuilder.elm
@@ -2,12 +2,12 @@ module SqlBuilder exposing (SelectQuery, build, exampleQuery)
 
 
 type alias SelectQuery =
-    { select : List Column
+    { select : List SelectExpression
     , from : Table
     }
 
 
-type Column
+type SelectExpression
     = All
     | AllFromTable TableIdentifier
     | Field String
@@ -45,9 +45,9 @@ tableToString table =
             table.name
 
 
-columnToString : Column -> String
-columnToString column =
-    case column of
+columnToString : SelectExpression -> String
+columnToString expression =
+    case expression of
         Field fieldName ->
             fieldName
 
